06cfe83967c8bc59f31676f749fd4a488338fe14
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2022-11-07 22:40:26 +0500

    1:30:49 -constructing nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,13 +24,41 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
 
 const NavBar = () =>
 {
-  const { activeMenu, setActiveMenu } = useStateContext()
+  const { activeMenu, setActiveMenu , isClicked,setIsClicked } = useStateContext()
+
+  const handleClick=()=>{
+
+  }
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative' >
       <NavButton title='Menu' customFunc={() =>setActiveMenu((prevState) => !prevState)} color='blue' icon={<AiOutlineMenu />} />
       <div className='flex' >
-      <NavButton title='Cart' customFunc={() =>handle} color='blue' icon={<AiOutlineMenu />} />
+      <NavButton title='Cart' customFunc={() =>handleClick('Cart')} color='blue' icon={<FiShoppingCart />} />
+      <NavButton title='Chat' customFunc={() =>handleClick('Chat')} dotColor='#03c9d7' color='blue' icon={<BsChatLeft />} />
+      <NavButton title='Notifications' customFunc={() =>handleClick('notification')} dotColor='#03c9d7' color='blue' icon={<RiNotification3Line />} />
+      <TooltipComponent content='profile' position='BottomCenter'>
+        <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-grey rounded-lg'>
+          <img src={avatar} 
+            className='rounded-full w-8 h-8'
+          />
+          <p>
+            <span className='text-grey-400 text-14' >Hi,</span>{' '}
+            <span className='text-grey-400 font-bold ml-1 text-14' >Michel</span>
+          </p>
+          <MdKeyboardArrowDown
+            className='text-grey-400 text-14'
+          />
+        </div>
+
+
+      </TooltipComponent>
+
+      {isClicked.cart && <Cart/>}
+      {isClicked.chat && <Chat/>}
+      {isClicked.notification && <Notification/>}
+      {isClicked.userProfile && <UserProfile/>}
+      {isClicked.cart && <Cart/>}
       </div>
     </div>
   )
