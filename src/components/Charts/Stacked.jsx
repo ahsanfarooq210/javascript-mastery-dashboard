46fadcbe4eb41519456f66bd4e32df13096a4d9e
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2022-11-09 16:47:27 +0500

    2:17:09 - Revenew section done now moving towards data tables section of the video

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -8,13 +8,19 @@ import { useStateContext } from '../../contexts/ContextProvider';
 const Stacked = ({width,height}) => {
   return (
     <ChartComponent
+    id='charts'
       width={width}
       height={height}
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
       chartArea={{border:{width:0}}}
-    >
+      tooltip={{enable:true}}
+      legendSettings={{background:'white'}}
+      >
       <Inject services ={[Legend,Category,StackingColumnSeries,Tooltip]}/>
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item,index)=>(<SeriesDirective key={index} {...item}/>))}
+      </SeriesCollectionDirective>
     </ChartComponent>
   )
 }
