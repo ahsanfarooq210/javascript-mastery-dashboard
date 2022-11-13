9d057694e87fce99826ec736f4a6e481237be88a
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2022-11-13 13:04:34 +0500

    added code to opening and closing nav bar items

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,66 +1,77 @@
-import React, {createContext,useContext,useState} from "react";
+import React, { createContext, useContext, useState } from "react";
 
-const StateContext = createContext()
+const StateContext = createContext();
 
-const initialState={
-    chat:false,
-    cart:false,
-    userProfile:false,
-    notification:false,
-}
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
 
-export const ContextProvider=({children})=>{
-    const [activeMenu, setActiveMenu] = useState(true)
-    const [isClicked, setIsClicked] = useState(initialState)
-    const [screenSize, setScreenSize] = useState(undefined)
-    const [currentColor, setCurrentColor] = useState('#03C9D7')
-    const [currentMode, setCurrentMode] = useState('Light')
-    const [themeSettings, setThemeSettings] = useState(false)
-    
-    const setMode=(event)=>{
-        setCurrentMode(event.target.value)
-        localStorage.setItem("themeMode", event.target.value)
-        setThemeSettings(false)        
+  const setMode = (event) => {
+    setCurrentMode(event.target.value);
+    localStorage.setItem("themeMode", event.target.value);
+    setThemeSettings(false);
+  };
+
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem("colorMode", color);
+    setThemeSettings(false);
+  };
+
+  const handleClick = (clicked) => {
+    let value;
+    switch (clicked) {
+      case "chat":
+        value = isClicked.chat;
+        break;
+      case "cart":
+        value = isClicked.cart;
+        break;
+      case "userProfile":
+        value = isClicked.userProfile;
+        break;
+      case "notification":
+        value = isClicked.notification;
+        break;
+      default:
+        value = false;
     }
+    setIsClicked({ ...initialState, [clicked]: !value });
+  };
 
-    const setColor=(color)=>{
-        setCurrentColor(color)
-        localStorage.setItem("colorMode", color)
-        setThemeSettings(false)            
-    }
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+        currentColor,
+        currentMode,
+        setCurrentColor,
+        setCurrentMode,
+        themeSettings,
+        setThemeSettings,
+        setMode,
+        setColor,
+      }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
 
-
-
-    const handleClick=(clicked)=>{
-        
-        setIsClicked({...initialState,[clicked]:true})
-    }
-
-    return (
-        <StateContext.Provider
-            value={{
-                activeMenu,
-                setActiveMenu,
-                isClicked,
-                setIsClicked,
-                handleClick,
-                screenSize,
-                setScreenSize,
-                currentColor,
-                currentMode,
-                setCurrentColor,
-                setCurrentMode,
-                themeSettings,
-                setThemeSettings,
-                setMode,
-                setColor,
-            }}
-        >
-            {children}
-        </StateContext.Provider>
-    )
-}
-
-
-export const useStateContext=()=>useContext(StateContext)
+export const useStateContext = () => useContext(StateContext);
